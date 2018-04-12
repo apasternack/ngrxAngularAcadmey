91e09ebc1fefe5e1393ad4bf7269f1752fdb2f59
Adam Pasternack <adam.pasternack@gmail.com>
2018-04-12 08:32:43 -0400

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
@@ -8,6 +8,7 @@ import { ThreadSectionComponent } from './thread-section/thread-section.componen
 import { MessageSectionComponent } from './message-section/message-section.component';
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { MessageListComponent } from './message-list/message-list.component';
+import { ThreadsService } from './services/threads.service';
 
 
 @NgModule({
@@ -20,9 +21,10 @@ import { MessageListComponent } from './message-list/message-list.component';
     MessageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ThreadsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/shared/to/all-user-data.ts
@@ -0,0 +1,8 @@
+import { Message } from './../model/message';
+import { Thread } from './../model/thread';
+import { Participant } from './../model/participant';
+export interface AllUserData {
+    participants: Participant[];
+    threads: Thread[];
+    messages: Message[];
+}
